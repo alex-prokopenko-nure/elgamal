a3d6145cafc6b54f013db1484a2a728b0672983f
alex-prokopenko-nure <oleksandr.prokopenko1@nure.ua>
2019-05-24 18:46:32 +0300

    init

---
+++ ElGamal/ElGamal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ElGamal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte[] bytes = new byte[513];
+            bytes[511] = 128;
+            BigInteger number = new BigInteger(bytes);
+            Console.WriteLine(number);
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ ElGamal/ElGamal/Services/ICryptoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace ElGamal.Services
+{
+    public interface ICryptoService
+    {
+        BigInteger GetBigPrime();
+    }
+}
